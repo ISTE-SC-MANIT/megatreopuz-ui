18a2c425d95da88c3d169f488bcc949ad6396bc8
jake <jjk848798@gmail.com>
2023-05-02 01:13:51 +0530

    fix:leaderboard

--- src/pages/dashboard/leaderboard.tsx
+++ src/pages/dashboard/leaderboard.tsx
@@ -130,7 +130,46 @@ const LeaderBoard: NextPage<ProtectedPageProps> = ({ viewer }) => {
 					<LoadingScreen loading={isLoading} />{' '}
 				</Box>
 			) : (
-				<>{<h1>Contest not started yet!</h1>}</>
+				<>
+					<div className={classes.root}>
+						<TableContainer component={Paper}>
+							<Table aria-label="caption table">
+								<caption>
+									Need to solve at least 1 question to make a place on leader
+									board
+								</caption>
+								<TableHead>
+									<TableRow>
+										<TableCell align="center">
+											<b>Rank</b>
+										</TableCell>
+										{/* <TableCell align="center">
+                      <b>Name </b>
+                    </TableCell> */}
+										<TableCell align="center">
+											<b>Username </b>
+										</TableCell>
+										<TableCell align="center">
+											<b>Questions Solved</b>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{data.getLeaderBoard.map((row, index) => (
+										<TableRow key={index}>
+											<TableCell align="center">{index + 1}</TableCell>
+											{/* <TableCell align="center">{row.name}</TableCell> */}
+											<TableCell align="center">{row.username}</TableCell>
+											<TableCell align="center">
+												{row.questionAttempted}
+											</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</div>
+				</>
 			)}
 		</div>
 	);
