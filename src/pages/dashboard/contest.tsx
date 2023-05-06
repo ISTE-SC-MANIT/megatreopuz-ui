faa40c679dca0361bdcb820c3f7e706495daa6dd
jake <jjk848798@gmail.com>
2023-05-06 16:35:57 +0530

    s

--- src/pages/dashboard/contest.tsx
+++ src/pages/dashboard/contest.tsx
@@ -360,10 +360,10 @@ const QuestionComponent: NextPage<ProtectedPageProps> = ({ viewer }) => {
 			) : (
 				<Box mt={20} className={classes.nullText}>
 					<Typography variant="h3" align="center">
-						{/* That`&apos;`s All for now. Stay tuned for next questions. we will be
-						back soon */}
+						That&apos;s All for now. Stay tuned for next questions. we will be
+						back soon
 						{/* The contest has Ended! We will get back to the winners soon*/}
-						Contest not started yet!
+						{/* Contest not started yet! */}
 					</Typography>
 				</Box>
 			)}
